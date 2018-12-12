d3d68437b7c9157d483717398391e0fac7aa0bd7
alonronin <alon@ronin.co.il>
2018-12-12 02:34:28 +0200

    :sparkle: add remote-spa app

---
+++ front/remote-spa/src/index.js
@@ -0,0 +1,35 @@
+import { init } from '@frontyjs/core';
+
+import './style.css';
+
+const reactApp = {
+  id: 'react',
+  onMount({ container, fronty }) {
+    const button = document.createElement('button');
+    button.innerText = 'Open Modal';
+    button.onclick = () => fronty.apps.get('modal').open(`Hello from React App!`);
+    container.appendChild(button);
+  }
+};
+
+const modalApp = {
+  id: 'modal',
+  type: 'js',
+  div: document.createElement('div'),
+  open(text) {
+    this.div.className = 'modal';
+    this.div.innerHTML = `<section><h1>${text}</h1><button>Close</button></section>`;
+    this.div.querySelector('button').addEventListener('click', this.close.bind(this));
+
+    document.body.prepend(this.div);
+  },
+  close() {
+    document.body.removeChild(this.div);
+  }
+};
+
+(async () => {
+  const fronty = await init(modalApp, reactApp);
+
+  console.log(fronty.apps);
+})();
---
+++ front/remote-spa/webpack.config.js
@@ -0,0 +1,39 @@
+const { resolve } = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+const context = resolve(__dirname, 'src');
+
+module.exports = {
+  entry: {
+    app: ['./']
+  },
+
+  context,
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      title: 'Orchestrator',
+      viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      template: 'index.html'
+    })
+  ],
+
+  devServer: {
+    proxy: [
+      {
+        context: ['/angularjs', '/angular2', '/vue', '/react'],
+        target: 'http://todomvc.com/examples',
+        changeOrigin: true
+      }
+    ]
+  }
+};
