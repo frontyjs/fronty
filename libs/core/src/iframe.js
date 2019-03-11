c6ceaa4a343152762ba81948facb1257a511d2a8
alonronin <alon@ronin.co.il>
2019-03-11 21:25:53 +0200

    :recycle: move iWindow to top

--- libs/core/src/iframe.js
+++ libs/core/src/iframe.js
@@ -28,8 +28,10 @@ export const iframe = async ({
     container.appendChild(iFrame);
   }
 
-  iFrame.contentWindow.fronty = fronty;
-  iFrame.name = id;
+  const iWindow = iFrame.contentWindow;
+
+  iWindow.fronty = fronty;
+  iFrame.setAttribute('fronty-app-name', id);
 
   const request = await fetch(url);
   const source = await request.text();
@@ -43,18 +45,16 @@ export const iframe = async ({
   iFrame.contentDocument.open().write($.html());
   iFrame.contentDocument.close();
 
-  const iWindow = iFrame.contentWindow;
-
   // Setup initial routing on inner app - so when loading a page such as
   // fronty/?app=url
   // The app frame would be loaded at #url
   // We fake a hashchange event so the inner framework can do its magic
   {
-    const url = parseQuerystring().get(name);
-    iWindow.history.replaceState({}, '', '#' + url);
+    const url = '#' + parseQuerystring().get(id);
+    iWindow.history.replaceState({}, '', url);
     const fakeEvent = new HashChangeEvent('hashchange', {
-      oldURL: '#' + url,
-      newURL: '#' + url
+      oldURL: '#',
+      newURL: url
     });
     iWindow.location.hash = url;
     iWindow.dispatchEvent(fakeEvent);
@@ -66,21 +66,21 @@ export const iframe = async ({
     console.log('onhashchange');
     const hash = iWindow.document.location.hash || '#';
     const qs = parseQuerystring();
-    qs.set(name, hash.substring(1));
+    qs.set(id, hash.substring(1));
     window.history.replaceState({}, '', qsToURL(qs));
   });
 
   // Support for onMount event inside apps
-  iFrame.contentWindow.addEventListener('DOMContentLoaded', e => {
+  iWindow.addEventListener('DOMContentLoaded', e => {
     const params = { container, fronty, app, frame: iFrame };
 
-    onMount = onMount || iFrame.contentWindow.onMount;
+    onMount = onMount || iWindow.onMount;
     if (onMount) onMount(params);
   });
 
   // When clicking "refresh iframe" in Chrome, the unload event is dispatched
   // - we catch that to handle our own iframe-displaying semantics
-  iFrame.contentWindow.addEventListener('unload', async e => {
+  iWindow.addEventListener('unload', async e => {
     await iframe({ id, container, url, fronty, app, replaceElement: iFrame });
   });
 };
