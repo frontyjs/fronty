1e94f803640cb6e2da7bfe9a3c632214cb8adb7f
configurator <configurator@google.com>
2019-05-05 23:25:16 +0300

    Finalize example

--- examples/hybrid-migration/modern/header/Menu.jsx
+++ examples/hybrid-migration/modern/header/Menu.jsx
@@ -3,13 +3,10 @@ import React from 'react';
 export const Menu = () => (
   <ul>
     <li>
-      <a href="#fronty/legacy/home">Home</a>
+      <a href="#fronty/legacy">Home</a>
     </li>
     <li>
-      <a href="#fronty/legacy/contact-us">Contact us</a>
-    </li>
-    <li>
-      <a href="#/about-us">About our wonderful company</a>
+      <a href="#fronty/legacy/!/contact-us">Contact us</a>
     </li>
   </ul>
 );
