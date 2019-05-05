71a745e49675e65cb387a94dda0feed433e410f4
alonronin <alon@ronin.co.il>
2019-05-06 00:38:43 +0300

    :sparkle: add todos example

---
+++ examples/todos/webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+const context = resolve(__dirname, 'src');
+
+module.exports = {
+  entry: {
+    app: ['./']
+  },
+
+  context,
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      title: 'Orchestrator',
+      viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      template: 'index.html'
+    })
+  ]
+};
