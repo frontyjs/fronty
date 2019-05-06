a25df2237430de405079d704e2b7ef270c2a32f5
configurator <configurator@google.com>
2019-05-06 16:35:21 +0300

    Change titles for todo apps

--- examples/todos/public/react/js/app.jsx
+++ examples/todos/public/react/js/app.jsx
@@ -151,7 +151,7 @@ var app = app || {};
       return (
         <div>
           <header className="header">
-            <h1>todos</h1>
+            <h1>react</h1>
             <input
               className="new-todo"
               placeholder="What needs to be done?"
