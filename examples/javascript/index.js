80bc819bab38c28f98326a337ab475f51d26032a
alonronin <alon@ronin.co.il>
2020-06-06 08:34:09 +0300

    :sparkle: javascript app example

--- examples/javascript/index.js
+++ examples/javascript/index.js
@@ -4,16 +4,18 @@ import './style.css';
 (async () => {
 	const app1Container = document.createElement('header');
 	const app2Container = document.createElement('main');
+	const app3Container = document.createElement('footer');
 
 	document.body.appendChild(app1Container);
 	document.body.appendChild(app2Container);
+	document.body.appendChild(app3Container);
 
 	const app1 = {
 		id: 'app1',
 		url: '/app1/',
 		container: app1Container,
-		onMount(...args) {
-			console.log(args);
+		onMount({ app }) {
+			console.log(app);
 		},
 	};
 
@@ -21,10 +23,19 @@ import './style.css';
 		id: 'app2',
 		url: '/app2/',
 		container: app2Container,
-		onMount(...args) {
-			console.log(args);
+		onMount({ app }) {
+			console.log(app);
 		},
 	};
 
-	const fronty = await init(app1, app2);
+	const app3 = {
+		id: 'app3',
+		url: '/app3/',
+		container: app3Container,
+		onMount({ frame, app }) {
+			console.log(app);
+		},
+	};
+
+	const fronty = await init(app1, app2, app3);
 })();
